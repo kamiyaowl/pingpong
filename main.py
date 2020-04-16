c6e47a0cbd888c0d98a6f328c1a13c5fae6f9a8b
K <kamiyaowl@gmail.com>
2020-04-17 01:00:23 +0900

    表が出た

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 import socket
+from rich.console import Console
+from rich.table import Column, Table
+
+console = Console()
 
 targets = [
     {"host": "localhost", "port": 135, },
@@ -6,13 +10,27 @@ targets = [
     {"host": "192.168.100.200", "port": 1111, },
     {"host": "192.168.1.1", "port": 22, },
 ]
+timeout_sec = 1
 
 def scan(host, port):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.settimeout(timeout_sec)
     ret = sock.connect_ex((host, port))
     sock.close()
     return ret == 0
 
-results = map(lambda t: scan(t["host"], t["port"]), targets)
-for r in results:
-    print(r)
+def scan_all_and_print(targets):
+    # header
+    table = Table(show_header=True, header_style="bold magenta")
+    table.add_column("host")
+    table.add_column("port")
+    table.add_column("alive")
+    # body
+    for t in targets:
+        resp = scan(t["host"], t["port"])
+        table.add_row(
+            t["host"], str(t["port"]), ("[green]OK[/green]" if resp else "[red]NG[/red]")
+        )
+    console.print(table)
+
+scan_all_and_print(targets)
